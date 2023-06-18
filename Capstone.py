245b37042244490fd86b461f455f4fd128f4ddb2
xdwctd100111 <delcidxav@gmail.com>
2023-06-18 18:56:06 -0400

    Update Capstone.py

--- Capstone.py
+++ Capstone.py
@@ -1,57 +1,200 @@
-
-from dotenv import load_dotenv
-import os
-import matplotlib as plt
-import datetime
 import psycopg2
-from faker import Faker
-import random
-from tqdm import tqdm
+
+import matplotlib.pyplot as plt
+
+
 
 
 class Database:
-    def __init__(self): #does not require 5 positional arguments
-        self.host = os.getenv('HOST')
-        self.port = os.getenv('PORT')
-        self.username = os.getenv('USER')
-        self.password = os.getenv('PASSWORD')
-        self.database = os.getenv('DATABASE')
 
-def connect(self):
-    error = None
-    self.connection = None
-    self.cur = None ### I changed self.cursor to self.cur
- 
-    if self.host and self.port and self.username and self.password and self.database: #if all these attributes arent none...
-        try:  #tries to get connnection to railway database
-            conn = psycopg2.connect(
-            host="containers-us-west-124.railway.app",
-            database="railway",
-            port=6105,
-            user="postgres",
-            password="nMph7vIhN07HdLO3PY2c"
-            )
-            self.cur = conn.cursor()
+    def __init__(self, host, port, username, password, database):
+
+        self.host = 'containers-us-west-124.railway.app'
+
+        self.port = 6105
+
+        self.username = 'postgres'
+
+        self.password = 'nMph7vIhN07HdLO3PY2c'
+
+        self.database = 'railway'
+
+        self.connection = None
+
+        self.cursor = None
+
+
+
+
+    def connect(self):
+
+        try:
+
             self.connection = psycopg2.connect(
+
                 host=self.host,
+
                 port=self.port,
+
                 user=self.username,
+
                 password=self.password,
+
                 database=self.database
+
             )
 
-        except: #if no connection, prints error message
-            print('failed to connect')
-def execute_query(self, query): #function to execute queries
-    try: 
-        self.cur.execute(query) ###I changed self.cursor to self.cur
-        self.connection.commit() #committing changes 
-        print('Query executed successfully')
-    except: 
-        psycopg2.Error
-def fetching_results(self):
-    return self.cur.fetchall()
+            self.cursor = self.connection.cursor()
 
+            print('Connected to the database successfully')
+
+        except psycopg2.Error as e:
+
+            print('Failed to connect to the database:', e)
+
+
+
+
+    def execute_query(self, query):
+
+        try:
+
+            self.cursor.execute(query)
+
+            self.connection.commit()
+
+            print('Query executed successfully')
+
+        except psycopg2.Error as e:
+
+            print('Error executing query:', e)
+
+    def fetch_results(self):
+
+        return self.cursor.fetchall()
+
+
+
+
+# Database connection details
+
+host = "containers-us-west-124.railway.app"
+
+port = 6105
+
+username = "postgres"
+
+password = "nMph7vIhN07HdLO3PY2c"
+
+database = "railway"
+
+
+
+
+# Check if the database exists
+
+def database_exists():
+
+    conn = psycopg2.connect(host=host, port=port, user=username, password=password)
+
+    conn.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
+
+    cur = conn.cursor()
+
+    cur.execute("SELECT 1 FROM pg_catalog.pg_database WHERE datname = %s", (database,))
+
+    exists = cur.fetchone()
+
+    cur.close()
+
+    conn.close()
+
+    return exists is not None
+
+
+
+
+def create_database():
+
+    conn = psycopg2.connect(host=host, port=port, user=username, password=password)
+
+    conn.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
+
+    cur = conn.cursor()
+
+    cur.execute(f"CREATE DATABASE {database}")
+
+    cur.close()
+
+    conn.close()
+# Create books table
+
+    cur.execute("""
+
+        CREATE TABLE IF NOT EXISTS books (
+
+            id SERIAL PRIMARY KEY,
+
+            title VARCHAR(255) NOT NULL,
+
+            author VARCHAR(255) NOT NULL,
+
+            category VARCHAR(255) NOT NULL
+
+        )
+
+    """)
+
+
+
+
+    # Create members table
+
+    cur.execute("""
+
+        CREATE TABLE IF NOT EXISTS members (
+
+            id SERIAL PRIMARY KEY,
+
+            name VARCHAR(255) NOT NULL
+
+        )
+
+    """)
+
+
+
+
+    # Create borrowed_books table
+
+    cur.execute("""
+
+        CREATE TABLE IF NOT EXISTS borrowed_books (
+
+            id SERIAL PRIMARY KEY,
+
+            member_id INT NOT NULL,
+
+            book_id INT NOT NULL,
+
+            borrowed_date DATE NOT NULL,
+
+            FOREIGN KEY (member_id) REFERENCES members (id),
+
+            FOREIGN KEY (book_id) REFERENCES books (id)
+
+        )
+
+    """)
+
+
+
+
+    conn.commit()
+
+    cur.close()
+
+    conn.close()
 
 ###SQL MEMBER CLASS
 
@@ -80,14 +223,13 @@ class Member: #function of member details
         results = self.db.fetch_results()
         return results
 
-database = Database()
+database = Database(host, port, username, password, database)
 
 
 ###SQL LIBRARY CLASS
 class Library:
-    def __init__(self):
-        self.database = Database()
-        self.database.connect() #connect to railway database
+    def __init__(self, db):
+        self.db = db
 
     def get_most_active_member(self):
         query = 'SELECT member_id FROM borrowed_books GROUP BY {member_id} ORDER BY COUNT(*) DESC LIMIT 1'
@@ -104,13 +246,104 @@ class Library:
         plt.title('Number of Books Borrowed Per Member')
         plt.show()
 
+    def visualize_borrowed_count_per_month(self):
+
+        query = "SELECT EXTRACT(MONTH FROM borrowed_date) AS month, COUNT(*) AS borrowed_count FROM borrowed_books GROUP BY month ORDER BY month"
+
+        self.db.execute_query(query)
+
+        results = self.db.fetch_results()
+
+        months = [result[0] for result in results]
+
+        counts = [result[1] for result in results]
+
+
+
+
+        plt.plot(months, counts, marker='o')
+
+        plt.xlabel("Month")
+
+        plt.ylabel("Number of Books Borrowed")
+
+        plt.title("Number of Books Borrowed per Month")
+
+        plt.show()
+
+
+
+
+    def get_active_members_per_month(self):
+
+        query = "SELECT EXTRACT(MONTH FROM borrowed_date) AS month, COUNT(DISTINCT member_id) AS active_members FROM borrowed_books GROUP BY month ORDER BY month"
+
+        self.db.execute_query(query)
+
+        return self.db.fetch_results()
+
+
+
+
+    def visualize_active_members_per_month(self):
+
+        results = self.get_active_members_per_month()
+
+        months = [result[0] for result in results]
+
+        counts = [result[1] for result in results]
+
+
+
+
+        plt.plot(months, counts, marker='o')
+
+        plt.xlabel("Month")
+
+        plt.ylabel("Number of Active Members")
+
+        plt.title("Number of Active Members per Month")
+
+        plt.show()
+
+
+
+
+    def visualize_borrowed_count_per_category(self):
+
+        query = "SELECT b.category, COUNT(*) AS borrowed_count FROM borrowed_books bb JOIN books b ON bb.book_id = b.id GROUP BY b.category"
+
+        self.db.execute_query(query)
+
+        borrowed_count = self.db.fetch_results()
+
+
+
+
+        categories = [item[0] for item in borrowed_count]
+
+        counts = [item[1] for item in borrowed_count]
+
+
+
+
+        plt.bar(range(len(categories)), counts)
+
+        plt.xticks(range(len(categories)), categories)
+
+        plt.xlabel("Category")
+
+        plt.ylabel("Number of Books Borrowed")
+
+        plt.title("Number of Books Borrowed per Category")
+
+        plt.show()
 
 db = Database(host, port, username, password, database)
 db.connect()
 library = Library(db)
 borrowed_count = [("Member 1", 10), ("Member 2", 5), ("Member 3", 8)]
 # Call visualize_borrowed_count method to display the graph
-library.visualize_borrowed_count(borrowed_count)
 
 
 ###SQL BOOK CLASS
@@ -130,7 +363,7 @@ class Book:
     
 
     def get_borrowed_books_per_month(self):
-        query = SELECT b.title COUNT(*) FROM books b JOIN           
+        query = 'SELECT b.title COUNT(*) FROM books b JOIN borrowed_books ON b.id = bb.book_id GROUP BY '
 
     #def get_active_members_per_month(self):
              
